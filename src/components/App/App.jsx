2d3606bf599dc079390da126b0fcc56a30558d91
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-04-17 20:06:08 +0300

    corect

---
+++ src/components/App/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+import useLocalStorage from '../hooks/useLocalStorage';
+import { ContactForm } from '../Form/Form';
+import { ContactList } from '../List/List';
+import { Filter } from '../Filter/Filter';
+import css from './App.module.css';
+
+const App = () => {
+  const [contacts, setContacts] = useLocalStorage('contacts', []);
+  const [filter, setFilter] = useState('');
+
+  const formSubmitHandler = formData => {
+    addToContacts(formData);
+  };
+
+  const addToContacts = contact => {
+    const normalizedName = contact.name.toLowerCase();
+    const isExist = contacts.some(
+      ({ name }) => name.toLowerCase() === normalizedName
+    );
+
+    if (isExist) {
+      return alert(`${contact.name} is already in contacts`);
+    }
+    const contactsList = contacts.concat(contact);
+    return setContacts(contactsList);
+  };
+
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
+  };
+
+  const getVisibleContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  const deleteContact = contactId => {
+    setContacts(contacts.filter(contact => contact.id !== contactId));
+  };
+  const visibleContacts = getVisibleContacts();
+
+  return (
+    <div className={css.container}>
+      <section className={css.section}>
+        <h1 className={css.title}>Phonebook</h1>
+        <ContactForm onSubmit={formSubmitHandler} />
+      </section>
+      <section className={css.section}>
+        <h2 className={css.title}>Contacts</h2>
+        <Filter value={filter} onChange={changeFilter} />
+        <ContactList
+          contacts={visibleContacts}
+          onDeleteContact={deleteContact}
+        />
+      </section>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,75 @@
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+import css from './Form.module.css';
+
+export const ContactForm = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const [id, setId] = useState(null);
+
+  const handleChange = e => {
+    const { name, value } = e.currentTarget;
+    const contactId = nanoid(4);
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+
+      case 'number':
+        setNumber(value);
+        break;
+
+      default:
+        return;
+    }
+    setId(contactId);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    onSubmit({ name, number, id });
+    reset();
+  };
+
+  const reset = () => {
+    setName('');
+    setNumber('');
+    setId(null);
+  };
+
+  return (
+    <form className={css.form} onSubmit={handleSubmit}>
+      <div className={css.field}>
+        <label className={css.label}>Name</label>
+        <input
+          className={css.input}
+          type="text"
+          name="name"
+          contactId={id}
+          value={name}
+          onChange={handleChange}
+          pattern="^[a-zA-Za-яА-Я]+(([' -][a-zA-Za-яА-Я ])?[a-zA-Za-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </div>
+      <div className={css.field}>
+        <label className={css.label}>Number</label>
+        <input
+          className={css.input}
+          type="tel"
+          name="number"
+          contactId={id}
+          value={number}
+          onChange={handleChange}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </div>
+      <button className={css.btn__submit} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
+};
